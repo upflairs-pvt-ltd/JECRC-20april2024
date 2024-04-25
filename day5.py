d6f91a42ee2db5045b7cb5c479ab2f1c0e3bae0c
Ranjit-Singh-786 <jiradhey402@gmail.com>
2024-04-25 15:16:45 +0530

    day5

---
+++ day5.py
@@ -0,0 +1,94 @@
+# available_balance = 5000
+# message = """
+# enter 1 for check balance,
+# enter 2 for deposit amount,
+# enter 3 for withdrawl amount
+# """
+# print(message)
+# task = int(input('enter your task :'))
+
+# if task == 1:
+#     # CHECK BALANCE
+#     print("your available balance is : ",available_balance)
+  
+    
+# elif task == 2:
+#     #DEPOSIT
+#     deposite_amount = int(input("Enter your amount for deposite :"))
+
+#     if deposite_amount>0:
+#         available_balance += deposite_amount
+#         print(f"You have successfully deposite your amount : {deposite_amount}")
+#         print("Your current available amount is : ",available_balance)
+#     else:
+#         print('plz enter valid amount ')
+    
+# elif task ==3:
+
+
+#     # withdrawl
+#     pass 
+
+# else:
+#     print("enter valid task")
+
+
+
+
+# <<<<<<<<<<< LOOP >>>>>>>>>>>>>>>>>
+
+# in range
+# for i in range(10):
+#     print(i)
+
+# lst = list(range(20))
+# lst = [10,20,30,40,50,60]
+# for item in lst:
+#     print(item)
+
+# tpl = (10,20,30,40,50,60)
+# company = "upflairs"
+# dt = {'name':'rohit','branch':'cse','roll_no':100}
+# for char in dt.values():
+#     print(char)
+
+
+# lst = [12,4,25,1,63,58,4,5,85,96,45,74,85,12]
+# target = int(input('enter your target : '))
+# for item in lst:
+#     if item == target:
+#         print('PRESENT')
+
+
+
+
+# lst = [12,4,25,1,63,58,4,5,85,96,45,74,85,12]
+# temp = 0
+# for item in lst:
+#     if item % 2 != 0:
+#         temp +=1
+# print(temp)
+
+#<<<<<<<<<<<<< WHILE LOOP >>>>>>>>>>>>>>>
+# i = 0
+# while i<10:
+#     print(i)
+#     i+=1
+
+
+
+# condition = True
+# while condition:
+#     print("hello world")
+#     condition = False
+
+# break , continue
+# for i in range(1,21):
+#     if i ==10:
+#         continue 
+#     print(i)
+
+
+# quizes
+# 1. find average of a given list
+# 2. find min and max element from a given list 
