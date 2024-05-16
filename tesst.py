d78b2b9b14da2e4c4a9113e28a265c89de08f782
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-05-16 16:34:29 +0530

    ml project

---
+++ tesst.py
@@ -0,0 +1,16 @@
+lis = [25,41,85,96,85,741]
+
+
+def add_two(num1,num2):
+    """
+    this is function that sum of two numbers,
+    numbers should be integer or float
+    """
+    return num1 + num2
+
+
+print(add_two.__doc__)
+
+
+
+
