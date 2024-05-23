36a73bc558bc7859f79f7a723aafc2c14fe9e11d
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-05-23 13:40:54 +0530

    23 may

--- ML-project2/app.py
+++ ML-project2/app.py
@@ -1,4 +1,8 @@
 from flask import Flask , render_template,url_for,request
+import joblib 
+bow = joblib.load('bow.lb')
+model = joblib.load('model.lb')
+
 
 app = Flask(__name__)
 
@@ -9,8 +13,15 @@ def home():
 @app.route('/predict' , methods= ['GET','POST'])
 def predict():
     if request.method == 'POST':
-        message = request.form['email_message']  
-        return message
+        message = request.form['email_message'] 
+        ls = [message]
+        x = bow.transform(ls) 
+
+        pred = model.predict(x)[0]
+        label = {'0':'ham','1':'spam'}
+        
+
+        return label[str(pred)]
 
 if __name__ =="__main__":
     app.run(host='0.0.0.0',port=2525,debug=True)
