5f6b57df70d728ced2e3e4001fbc47b1a492234a
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-10-19 09:09:11 +0530

    chatbot code

--- chatbot/app.py
+++ chatbot/app.py
@@ -2,9 +2,21 @@ from dotenv import load_dotenv
 from PIL import Image    # pillow 
 import os 
 import streamlit as st  # to create the interface
+import google.generativeai as genai   # generativeAI
 load_dotenv() 
 
 api_key = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=api_key)
+
+def get_response_from_gemini(question,image):
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    if question != "":
+        response = model.generate_content([question,image])
+    else:
+        response = model.generate_content(image) 
+    return response.text 
+
+
 
 st.set_page_config(page_title="Gemini chatbot") 
 st.header("Gemini LLM application 🤖🤖")
@@ -22,3 +34,6 @@ if uploaded_file is not None:
 
 submit = st.button("submit")
 
+
+if submit:
+    get_response_from_gemini(question=question,image=imge)
